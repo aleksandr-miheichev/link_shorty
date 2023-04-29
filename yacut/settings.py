9cd611adba42324534c853b3dad3e89330b09390
Aleksandr Miheichev <ya.saska20@gmail.com>
2023-04-29 12:08:42 +0300

    Sprint_21_iteration_version_1.6

--- yacut/settings.py
+++ yacut/settings.py
@@ -9,7 +9,7 @@ LENGTH_ERROR = 'Длина должна быть от 1 до 16 символов
 RULE = r'^[a-zA-Z0-9\-_]*$'
 INVALID_NAME_LINK = 'Указано недопустимое имя для короткой ссылки'
 ID_AVAILABLE = 'Имя {custom_id} уже занято!'
-ID_AVAILABLE_API = 'Имя {custom_id} уже занято'
+ID_AVAILABLE_API = 'Имя {custom_id} уже занято.'
 
 
 class Config(object):
